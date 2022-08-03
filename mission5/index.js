504f220994c655af825c3f55a739c2337cbd4e8e
godkais <81160424+godkais@users.noreply.github.com>
2022-08-03 18:10:00 +0900

    mission5

---
+++ mission5/index.js
@@ -0,0 +1,9 @@
+var bool=true
+var num= 5
+var str="언제 다 하누"
+var undefine
+var nu =null
+var sym= Symbol('x')
+var list=[1,2,3]
+var bigint=9n
+console.log(bool,num,str,undefine,nu,sym,list,bigint)
